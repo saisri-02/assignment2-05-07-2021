1594abcb06048451b9ebad909eaa1a70bcc275a3
saisri-02 <71486510+saisri-02@users.noreply.github.com>
2021-07-05 23:24:13 +0530

    initial commit

---
+++ Sumofarray.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Sumofarray
+{
+
+public static void main(String[] args)
+{
+
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter size of array : ");
+	
+int n=sc.nextInt();
+
+int a[]=new int[n];
+
+System.out.println("Enter values into array : ");
+
+for(int i=0;i<n;i++)
+
+{
+  
+a[i]=sc.nextInt();
+
+}
+int sum=0;
+	
+for(int i=0;i<n;i++)
+{  
+sum+=a[i];
+
+}
+
+System.out.println("Sum = "+sum);
+
+}
+}
